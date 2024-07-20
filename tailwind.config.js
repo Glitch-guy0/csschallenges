07dcde6be5245c6ea9f9d29915ed3440317a713e
Glitch-guy0 <prajwalm9845@gmail.com>
2024-07-20 16:53:17 +0530

    challenge 5 completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "180spin": "half-spin 1s ease-out infinite"
+      },
+      keyframes:{
+        "half-spin":{
+          "0%": {
+            transform: "rotate(45deg)"
+          },
+          "100%":{
+            transform: "rotate(225deg)"
+          }
+        }
+      }
+    },
   },
   plugins: [],
 }
